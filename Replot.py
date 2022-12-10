a56691d61b5e3614d6fa8077325be707595b5705
LouisW <114333810+bruh-guy64@users.noreply.github.com>
2022-12-10 15:37:57 +0800

    Add files via upload

---
+++ Replot.py
@@ -0,0 +1,89 @@
+from plot_library import *
+import glob , cv2 , os
+#path = "C:/Users/Student/OneDrive/桌面/JetBot/Garbage/dataset2/apex/"
+#filelist = find_jpg(path)
+def click_event_racer(event , x , y , flags , param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global new_img
+        name = filelist[pos]
+        posx = lenofName(name , 0 , "_")
+        new_name = rename_jetracer(name , str(x) , posx , 0)
+        posx_new = lenofName(new_name , 0 , "_")
+        posy_new = lenofName(new_name , (posx_new+1) , "_")
+        new_name = rename_jetracer(new_name , str(y) , posy_new , (posx_new+1))
+        os.rename(name , new_name)
+        filelist[pos] = new_name
+        print("New name : "+new_name)
+def click_event_bot(event , x , y , flags , param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        name = filelist[pos]
+        posx , posy = [] , []
+        for i in str(x):
+            posx.append(i)
+        for i in str(y):
+            posy.append(i)
+        posx , posy = shifting(str(x) , 3) , shifting(str(y) , 3)
+        new_name = edit_name(name,posx,posy,3,3,7)
+        os.rename(name , new_name)
+        filelist[pos] = new_name
+        print("Name name : "+new_name)
+def ask(qus):
+    print(qus)
+    inp = input()
+    if len(inp) != 0:
+        return inp
+def plot_jetbot():
+    for i in range(len(filelist)):
+        name = filelist[i]
+        global pos
+        pos = i
+        print(name)
+        x = get_current_xy(name , 3 , 3) 
+        y = get_current_xy(name , 3 , 7)
+        img = cv2.imread(name , cv2.IMREAD_UNCHANGED)
+        global new_img
+        new_img = img
+        draw_img(img , int(x) , int(y))
+        cv2.imshow("Img -JetBot" , img)
+        cv2.setMouseCallback("Img -JetBot" , click_event_bot)
+        cv2.waitKey(0)
+def plot_racer():
+    for i in range(len(filelist)):
+        name = filelist[i]
+        global pos
+        pos = i
+        posx = lenofName(name , 0 , "_")
+        posy = lenofName(name , (posx+1) , "_")
+        current_x = get_current_xy(filelist[i] , posx , 0)
+        current_y = get_current_xy(filelist[i] , posy , posx+1)
+        img = cv2.imread(filelist[i] , cv2.IMREAD_UNCHANGED)
+        draw_img(img , int(current_x) ,int(current_y))
+        cv2.imshow("Img -JetRacer" , img)
+        cv2.setMouseCallback("Img -JetRacer" , click_event_racer)
+        cv2.waitKey(0)
+def main():
+    global path
+    path = ask("Path :")
+    path = path.replace("\\" , "/")
+    tem_list = []
+    for i in path:
+        tem_list.append(i)
+    tem_list.reverse()
+    if tem_list[0] != "/":
+        path += "/"
+    try:
+        find_jpg(path)
+    except BaseException:
+        print("Syntax Error")
+        return 0
+    else:
+        global filelist
+        filelist = find_jpg(path)
+    ans = ask("Mode for plot [jb/jr] :")
+    if ans == "jr":
+        plot_racer()
+    if ans == "jb":
+        plot_jetbot()
+if __name__ =="__main__" :
+    while True:
+        main()
---
+++ plot_library.py
@@ -0,0 +1,88 @@
+import cv2 , glob , os
+def find_jpg(file_path):
+    tem_list = []
+    file_path = file_path.replace("\\" , "/")
+    os.chdir(file_path)
+    tem_list = glob.glob("*jpg")
+    return tem_list
+def draw_img(img , x ,y):
+    img = cv2.circle(img , (x ,y) , 8 , (0,255,0) , 3)
+    return img
+def shift(target_len , var):
+    tem_list = []
+    for i in range(target_len - len(var)):
+        tem_list.append("0")
+    for i in var:
+        tem_list.append(i)
+    return tem_list
+def show_img(file_path):
+    img = cv2.imread(file_path , cv2.IMREAD_UNCHANGED)
+    cv2.imshow("Img" , img)
+    cv2.waitKey(0)
+def newname(old , var , startvar , len):
+    tem_list , tem_str= [] , ""
+    for i in old:
+        tem_list.append(i)
+    for i in range(len):
+        tem_list[i+startvar] = var[i]
+    tem_str = "".join(tem_list)
+    return tem_str
+def get_current_xy(file_name , len , startvar):
+    tem_list = []
+    var = ""
+    for i in file_name:
+        tem_list.append(i)
+    for i in range(len):
+        var += tem_list[i+startvar]
+    return var
+def lenofName(name , startvar , endstr):
+    tem_list = []
+    pos = 0
+    for i in name:
+        tem_list.append(i)
+    for i in range(10):
+        if tem_list[i+startvar] == endstr:
+            return pos
+        else:
+            pos+=1
+def rename_jetracer(name , var , varlen , startvar):
+    tem_list = []
+    var_list = []
+    tem_str = ""
+    tem_int = 0
+    for i in name:
+        tem_list.append(i)
+    for i in range(varlen):
+        tem_list.pop(startvar)
+    for i in var:
+        var_list.append(i)
+    var_list.reverse()
+    for i in var_list:
+        tem_list.insert((startvar) , i)
+        tem_int += 1
+    tem_str = "".join(tem_list)
+    return tem_str
+def shifting(xy_list,xy_len):
+    xy_list=str(xy_list)
+    tem_xylist=[]
+    for i in xy_list:
+        tem_xylist.append(i)
+    tem_list=[]
+    starting_pos=0
+    starting_pos=(xy_len-(len(tem_xylist)))
+    for i in range(starting_pos):
+        tem_list.append("0")
+    for i in tem_xylist:
+        tem_list.append(i)
+    return tem_list
+def edit_name(img_name,x,y,xy_len,start_x,start_y):
+    tem_list=[]
+    tem_str=""
+    for i in img_name:
+        tem_list.append(i)
+    for i in range(xy_len):
+        tem_list[(start_x)+i]=x[i]
+    for i in range(xy_len):
+        tem_list[(start_y)+i]=y[i]
+    tem_str="".join(tem_list)
+    return tem_str
